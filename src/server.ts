8d41e3c6eb9c6a380a79a0331af33658fdcc9454
Laise <laisevn@gmail.com>
2024-01-29 17:25:58 -0300

    chore: prisma configuration

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
-const express = require('express')
+import express from 'express'
 
 const api = express()
 
-api.get('/', (req, res)=> {
-    console.log('hello from express')
-    res.status(200)
-    res.json({message: 'hello'})
+api.get('/', (req: express.Request, res: express.Response) => {
+  console.log('hello from express')
+  res.status(200)
+  res.json({ message: 'hello' })
 })
 
-api.listen(3000, ()=> {
-    console.log('hello on localhost')
+api.listen(3000, () => {
+  console.log('hello on localhost')
 })
 
 module.exports = api
